ca052793b5af4eb2a632e6339dbe1e7fd2752c52
kawada.kyosuke.mc00359@memberscareer.co.jp <kawada.kyosuke.mc00359@memberscareer.co.jp>
2020-09-05 11:58:15 +0900

    first commit

---
+++ app/Http/Controllers/Api/ContactController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactMail;
+
+class ContactController extends Controller
+{
+    public function index(Request $request) {
+        $to = $request->input('email');
+        Mail::to($to)->send(new ContactMail(['name' => $request->input('name'), 'email' => $request->input('email'), 'text' => $request->input('text')]));
+        return response()->json([
+          'name' => $request->input('name'),
+          'email' => $request->input('email'),
+          'text' => $request->input('text'),
+      ], 200);
+      }
+
+
+      public function test(Request $request) {
+        return response()->json([
+          'test' => 'test',
+          $request->input('name')
+      ], 200);
+      }
+}
---
+++ resources/views/contact/inquiry_mail.blade.php
@@ -0,0 +1,3 @@
+名前<br>{{$inquiry['name']}}さん
+<br>
+お問い合わせ内容<br>{{$inquiry['text']}}
